fd1dc64fdd0ae34eefe350823b15981f5e1b2978
AnaMilpalm <168805606+AnaMilpalm@users.noreply.github.com>
2024-09-22 18:02:29 +0000

    Deploying to gh-pages from @ AnaMilpalm/goit-js-hw-09@bfeae59b8de4e143b56512986ed85576ed34a55b 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -8,5 +8,5 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as t}from"./assets/
      />
    </a>
  </li>
- `}).join("");n.insertAdjacentHTML("beforeend",c);let s=new t(".gallery a",{caption:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250});s.on("show.simplelightbox");
+ `}).join("");n.insertAdjacentHTML("beforeend",c);const s=new t(".gallery a",{caption:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250});s.on("show.simplelightbox");
 //# sourceMappingURL=1-gallery.js.map
